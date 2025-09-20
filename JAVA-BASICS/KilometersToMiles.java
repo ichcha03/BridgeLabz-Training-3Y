5435de8e1860625d2b2a4339bfe7b15afb9f04d6
Ichcha <mehrishiichcha@gmail.com>
2025-09-20 23:58:47 +0530

    java basics

---
+++ JAVA-BASICS/KilometersToMiles.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+
+import java.util.Scanner;
+
+public class KilometersToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter distance in Kilometers: ");
+        double kilometers = sc.nextDouble();
+
+        double miles = kilometers * 0.621371;
+
+        System.out.println(kilometers + " km = " + miles + " miles");
+        sc.close();
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# OOPS Concepts in Java
-
-This branch contains programs, examples, and explanations related to **Object-Oriented Programming System (OOPS)** concepts in Java.
